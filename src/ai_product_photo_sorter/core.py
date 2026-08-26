f5fba75e7d2c0252c91975050b48d6a2175dbe99
Mohamed Anwar <mhmdwaelanwr@gmail.com>
2026-08-26 12:14:47 +0300

    Wire smart Markdown report into shared core

--- src/ai_product_photo_sorter/core.py
+++ src/ai_product_photo_sorter/core.py
@@ -4,11 +4,13 @@ from .paths import env_file, requirements_file
 from . import _core_impl as _impl
 from .hardening import apply_hardening
 from .dynamic_taxonomy import apply_dynamic_taxonomy
+from .smart_report import apply_smart_report
 
 _impl.DEFAULT_ENV_FILE = env_file()
 _impl.REQUIREMENTS_FILE = requirements_file()
 apply_hardening(_impl)
 apply_dynamic_taxonomy(_impl)
+apply_smart_report(_impl)
 
 globals().update({name: getattr(_impl, name) for name in dir(_impl) if not name.startswith("_")})
 main = _impl.main
